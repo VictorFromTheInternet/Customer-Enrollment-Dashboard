6203899ab155d8009995536b64b40dacba6933bd
VictorFromTheInternet <vma0430@gmail.com>
2026-02-01 12:41:39 -0600

    testing out the shadcn components working on the layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import {Button} from '@/components/ui/button' 
+
 import './App.css'
 
 function App() {
@@ -8,26 +11,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input type="text" name="" id="" value={`${count}`} disabled/>
+
+
+      <div className="flex flex-center justify-center align-center gap-2">
+        <Button onClick={()=>{setCount((prev) => prev+1)}}>
+          Increment
+        </Button>
+
+        <Button onClick={()=>{setCount((prev) => prev-1)}}>
+          Decrement
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
